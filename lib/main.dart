68cbdc1d8d3572553c75ee37023ccede5e5b3df7
aryanyadav9 <aryanyadav9.2000@gmail.com>
2022-05-06 15:07:44 +0530

    added home folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'Covid Tracking and News App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
